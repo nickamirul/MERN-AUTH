6286f90b5129cea0808b111b5f8623241672545e
nickamirul <nickamirul9945@gmail.com>
2025-02-11 19:10:38 +0800

    remove border in signup input

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -6,11 +6,11 @@ const SignUp = () => {
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
       <form className='flex flex-col gap-4'>
         <input type='text' placeholder='username' 
-        className='border p-3 rounded-lg bg-white' id='username'/>
+        className='p-3 rounded-lg bg-white' id='username'/>
         <input type='email' placeholder='email' 
-        className='border p-3 rounded-lg bg-white' id='email'/>
+        className='p-3 rounded-lg bg-white' id='email'/>
         <input type='password' placeholder='password' 
-        className='border p-3 rounded-lg bg-white' id='password'/>
+        className='p-3 rounded-lg bg-white' id='password'/>
         <button className='bg-slate-700 text-white 
         p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80  '>
           Sign Up</button>
