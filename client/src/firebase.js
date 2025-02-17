d82d11661827b5f751548fa4bb912bb0110e5421
nickamirul <nickamirul9945@gmail.com>
2025-02-17 19:29:59 +0800

    Complete image upload functionality

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -4,13 +4,22 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//   authDomain: "mern-auth-1ffa2.firebaseapp.com",
+//   projectId: "mern-auth-1ffa2",
+//   storageBucket: "mern-auth-1ffa2.firebasestorage.app",
+//   messagingSenderId: "1077085156858",
+//   appId: "1:1077085156858:web:1788afba985613a4342043"
+// };
+
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "mern-auth-1ffa2.firebaseapp.com",
-  projectId: "mern-auth-1ffa2",
-  storageBucket: "mern-auth-1ffa2.firebasestorage.app",
-  messagingSenderId: "1077085156858",
-  appId: "1:1077085156858:web:1788afba985613a4342043"
+  authDomain: "mern-estate-ae1e9.firebaseapp.com",
+  projectId: "mern-estate-ae1e9",
+  storageBucket: "mern-estate-ae1e9.appspot.com",
+  messagingSenderId: "589129740434",
+  appId: "1:589129740434:web:8a35f4a862ac221580764b"
 };
 
 // Initialize Firebase
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,18 +1,118 @@
 import { useSelector } from "react-redux";
+import { useRef, useState, useEffect } from "react";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
+import { app } from "../firebase";
 
 const Profile = () => {
   const { currentUser } = useSelector((state) => state.user);
+  const fileRef = useRef(null);
+  const [file, setFile] = useState(undefined);
+  const [filePercent, setFilePercent] = useState(0);
+  const [fileUploadError, setFileUploadError] = useState(false);
+  const [formData, setFormData] = useState({});
+
+  //firebase storage code, put this in the firebase console
+  // service firebase.storage {
+  //   match /b/{bucket}/o {
+  //     match /{allPaths=**} {
+  //       allow read;
+  //       allow write:if
+  //       request.resource.size < 2 * 1024 * 1024 &&
+  //       request.resource.contentType.matches('image/.*')
+  //     }
+  //   }
+  // }
+
+  useEffect(() => {
+    if (file) {
+      handleFileUpload(file);
+    }
+  }, [file]);
+
+  const handleFileUpload = (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log("Upload is " + progress + "% done");
+        setFilePercent(Math.round(progress));
+      },
+      (error) => {
+        setFileUploadError(true);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>
+          setFormData({ ...formData, avatar: downloadURL })
+        );
+      }
+    );
+  };
+
   return (
     <div>
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
       <form className="flex flex-col gap-4 max-w-md mx-auto">
-        <img src={currentUser.avatar} alt="profile" className="h-24 w-24 object-cover rounded-full mx-auto" />
+        <input
+          onChange={(e) => setFile(e.target.files[0])}
+          type="file"
+          ref={fileRef}
+          hidden
+          accept="image/*"
+        />
+        <img
+          onClick={() => fileRef.current.click()}
+          src={formData.avatar || currentUser.avatar}
+          alt="profile"
+          className="h-24 w-24 object-cover rounded-full mx-auto"
+        />
+        <p className="text-center">
+          {fileUploadError ? (
+            <span className="text-red-700">Error uploading image
+            (image must be less than 2 mb)</span>
+          ) : filePercent > 0 && filePercent < 100 ? (
+            <span className="text-slate-700">{`Uploading ${filePercent}%`}</span>
+          ) : filePercent === 100 ? (
+            <span className="text-green-700">Uploaded successfully</span>
+          ) : (
+            ""
+          )}
+        </p>
         <div className="flex flex-col gap-2 my-2">
-          <input type="text" id="username" placeholder="Username" className="bg-white outline-none p-3 rounded-lg" />
-          <input type="text" id="email" placeholder="Email" className="bg-white outline-none p-3 rounded-lg" />
-          <input type="text" id="password" placeholder="Password" className="bg-white outline-none p-3 rounded-lg" />
+          <input
+            type="text"
+            id="username"
+            defaultValue={currentUser.username}
+            placeholder="Username"
+            className="bg-white outline-none p-3 rounded-lg"
+          />
+          <input
+            type="text"
+            id="email"
+            defaultValue={currentUser.email}
+            placeholder="Email"
+            className="bg-white outline-none p-3 rounded-lg"
+          />
+          <input
+            type="text"
+            id="password"
+            placeholder="Password"
+            className="bg-white outline-none p-3 rounded-lg"
+          />
         </div>
-        <button className="bg-slate-700 text-white p-3 rounded-lg hover:opacity-95">Update</button>
+        <button className="bg-slate-700 text-white p-3 rounded-lg hover:opacity-95">
+          Update
+        </button>
       </form>
       <div className="flex justify-between max-w-md mx-auto my-5">
         <span className="text-red-700 cursor-pointer">Delete Account</span>
